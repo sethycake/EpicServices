c78eef12c77d4cc9fe6c0b3ff3b9931d93978c84
sethycake <151950673+sethycake@users.noreply.github.com>
2024-01-10 09:38:39 +0100

    Add files via upload

---
+++ EpicServices.py
@@ -0,0 +1,132 @@
+try:
+    import webbrowser
+    import time
+    from time import sleep
+    import requests
+    import os
+except ModuleNotFoundError:
+    print("Couldn't import packages..")
+    os.system("pause")
+
+def home():
+    print('''
+    
+
+███████╗██████╗ ██╗ ██████╗    ███████╗███████╗██████╗ ██╗   ██╗██╗ ██████╗███████╗███████╗
+██╔════╝██╔══██╗██║██╔════╝    ██╔════╝██╔════╝██╔══██╗██║   ██║██║██╔════╝██╔════╝██╔════╝
+█████╗  ██████╔╝██║██║         ███████╗█████╗  ██████╔╝██║   ██║██║██║     █████╗  ███████╗
+██╔══╝  ██╔═══╝ ██║██║         ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██║██║     ██╔══╝  ╚════██║
+███████╗██║     ██║╚██████╗    ███████║███████╗██║  ██║ ╚████╔╝ ██║╚██████╗███████╗███████║
+╚══════╝╚═╝     ╚═╝ ╚═════╝    ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝ ╚═════╝╚══════╝╚══════╝
+                                                                                                                                                 
+»-----------------------------------------------•----------------------------------------------------------------------------«
+Discord Tools                               OP TOOLS                      DOXBIN                   OTHER THINGS
+{1}DMGO                                     {6}Fsociety                   {11}Doxbin.org           {16}Blackbox             
+{2}GANG-nuker                               {7}Seeker                     {12}Doxbin.com           {17}Ip checker
+{3}ATIO                                     {8}BLX                        {13}Doxbin.net           {18}Ip stresser site
+{4}DiscordRAT                               {9}Osintgram                  {14}Doxbin Telegram      {19}How sicure is my pass?
+{5}DiscordRAT 2.0                           {10}Illegal Services          {15}DoxTracker           {20}Checkusarnames 
+»-----------------------------------------------•----------------------------------------------------------------------------«
+                                                READ IMPORTANT.txt
+    ''')
+    choice = input('>>>')
+    if choice == '1':
+        webbrowser.open('https://github.com/V4NSH4J/discord-mass-DM-GO')  
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '2':
+        webbrowser.open('https://github.com/TT-Tutorials/GANG-Nuker')    
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '3':
+        webbrowser.open('https://github.com/AstraaDev/Discord-All-Tools-In-One')   
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '4':
+        webbrowser.open('https://github.com/Sp00p64/DiscordRAT')    
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '5':
+        webbrowser.open('https://github.com/moom825/Discord-RAT-2.0')    
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '6':
+        webbrowser.open('https://github.com/Manisso/fsociety')   
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '7':
+        webbrowser.open('https://github.com/thewhiteh4t/seeker')  
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '8':
+        webbrowser.open('https://github.com/blxstealer/BLX-Stealer')   
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '9':
+        webbrowser.open('https://github.com/Datalux/Osintgram')   
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '10':
+        webbrowser.open('https://github.com/Illegal-Services/Illegal_Services')
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()   
+    elif choice == '11':
+        webbrowser.open('htpps://doxbin.org')
+        time.sleep(5)
+        print("returning in 3 seconds")
+        home()
+    elif choice == '12':
+        webbrowser.open('htpps://doxbin.com')
+        time.sleep(5)
+        print("returning in 3 seconds")
+        home()    
+    elif choice == '13':
+        webbrowser.open('htpps://doxbin.net')
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '14':
+        webbrowser.open('https://t.me/doxbin')
+        print("returning in 3 seconds")
+        time.sleep(5)
+        home()
+    elif choice == '16':
+        webbrowser.open('https://www.blackbox.ai/')
+        time.sleep(5)
+        home()
+    elif choice == '17':
+        print('https://ipinfo.io/')
+        time.sleep(5)
+        home()
+    elif choice == '18':
+        webbrowser.open('https://stresser.st/')
+        time.sleep(5)
+        home()
+    elif choice == '19':
+        webbrowser.open('https://www.security.org/how-secure-is-my-password/')
+        time.sleep(5)
+        home()
+    elif choice == '15':
+        webbrowser.open('https://github.com/KURO-CODE/DoxTracker')
+        time.sleep(5)
+        home()
+    elif choice == '20':
+        webbrowser.open('https://checkusernames.com')
+        time.sleep(5)
+        home()
+    else:
+        print("Not a valid option")
+        home()
+home()
+
+
